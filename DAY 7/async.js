1d3d9a82b9927eb82f3c15008080ff2dba90bad7
ArulventhanGM <arulventhangm@gmail.com>
2025-06-24 10:22:57 +0530

    Add initial implementation of async functionality, CSS styles for card layout, and HTML structure

---
+++ DAY 7/async.js
@@ -0,0 +1,40 @@
+// let count=0;
+
+// let timer = setInterval(()=> 
+//     {count++;
+//         console.log("Hello agian");
+//     if(count>5){
+//     clearInterval(timer);
+// }
+// },2000);
+
+
+// console.log("hello form async");
+// function doTask(taskName, callback) {
+//   console.log(`Starting ${taskName}....`);
+//   setTimeout(() => {
+//     console.log(`${taskName} completed!`);
+//     callback();
+//   }, 5000);
+// }
+
+// doTask("Regading Book", ()=>{
+//   console.log("Now reading chapter 10");
+// });
+let order = new Promise((resolve, reject) => {
+  let isOverWorking = true;
+  setTimeout(() => {
+    if(isOverWorking) {
+      resolve("Order is ready");
+    } else {
+      reject("Order delay");
+    }
+  }, 3000);
+});
+
+order
+  .then((message) => console.log("Success: ",message))
+  .catch((error) => {
+    console.log("Error: ",error);
+  });
+  
